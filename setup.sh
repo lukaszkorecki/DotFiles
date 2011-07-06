1168b59abe9dad79439dfddad5e4243426cae303
Łukasz Korecki <lukasz@coffeesounds.com>
2011-07-06 21:35:30 +0100

    full setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-echo Moving from `pwd` to home
+echo Going home
 cd ~
+
+echo Cleaning up
+rm -rf .vim
+rm .vimrc
+
 echo Cloning
 git clone git://github.com/lukaszkorecki/VimConfig.git .vim
-echo Symlinking .vimrc and .vim
-ln -s .vim/.vimrc .vimrc
-echo done
+
+echo Symlinking .vimrc and
+ln -s ~/.vim/.vimrc .vimrc
+
+echo Getting submodules
+cd .vim
+git submodule init
+
+git submodule update
+
+echo DONE
