150f80635997baf638895a6cd47ab71b72f4bfdd
Łukasz Korecki <lukasz@coffeesounds.com>
2012-06-11 11:18:37 +0100

    better, improved mailview

--- bins/mailview.zsh
+++ bins/mailview.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 # Open an html attachement in your browser of choice
 # OSX only, but y'know... can be tweaked
-local fn=/tmp/mail.$RANDOM.html
-tee $fn && open $fn
+# (s/open/xdg-open)
+tee /tmp/$1 > /dev/null && open /tmp/$1
