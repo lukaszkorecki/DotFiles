df2998bb1179dd52fcd6ed7f9badc536775c2088
Łukasz Korecki <lukasz@coffeesounds.com>
2012-11-28 15:04:34 +0000

    update remove-submodule script

--- remove-submodule.sh
+++ remove-submodule.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # ./remove-submodule name path-to-sm
-echo Removing $1 from $2
+echo Removing submodule: $1 from $2
+
+echo Removed entries from .git configs
 grep -v $1  .gitmodules > .gitmodules.2 && mv .gitmodules.2 .gitmodules
+echo "gitmodules done"
+grep -v $1  .git/config > .git/config.2 && mv .git/config.2 .git/config
+echo ".git/config done"
+
+echo "removing files"
 git rm -r --cached $2
+echo git files done
 rm -r $2
+echo untracked files done
