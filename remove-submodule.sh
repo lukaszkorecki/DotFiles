d22199147fc7ccab12e2a339bfcf90c555009365
Łukasz Korecki <lukasz@coffeesounds.com>
2013-04-01 14:49:21 +0100

    vim: remove unused plugins add scala support and better tmux integration plugin

--- remove-submodule.sh
+++ remove-submodule.sh
@@ -3,13 +3,13 @@
 echo Removing submodule: $1 from $2
 
 echo Removed entries from .git configs
-grep -v $1  .gitmodules > .gitmodules.2 && mv .gitmodules.2 .gitmodules
+grep -iv $1  .gitmodules > .gitmodules.2 && mv .gitmodules.2 .gitmodules
 echo "gitmodules done"
-grep -v $1  .git/config > .git/config.2 && mv .git/config.2 .git/config
+grep -iv $1  .git/config > .git/config.2 && mv .git/config.2 .git/config
 echo ".git/config done"
 
 echo "removing files"
-git rm -r --cached $2
+git rm -fr --cached $2
 echo git files done
 rm -r $2
 echo untracked files done
