d34f0990bd19683573e2acc4b4a6fd235f18d8eb
Łukasz Korecki <lukasz@coffeesounds.com>
2013-09-06 09:14:05 +0100

    Script for attaching and rotating 2nd monitor if it's present

---
+++ xres/2ndmonitor.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [[ -z "$(xrandr | grep 'DP-1 disconn')" ]] ; then
+  # use 2nd monitor
+  xrandr --output DP-1 --auto --right-of LVDS-2
+  # ...and rotate it
+  xrandr --output DP-1 --rotate left
+fi
+
