f291cb00ccc95b13a49234906eb8d87633d9cf79
Łukasz Korecki <lukasz@coffeesounds.com>
2013-09-19 21:17:22 +0100

    use urxvt as terminal + important packages script

---
+++ ubuntu_packages.sh
@@ -0,0 +1,12 @@
+sudo apt-get install tmux \
+                    vim \
+                    mutt-patched \
+                    i3 \
+                    xfont-terminus \
+                    console-terminus \
+                    rxvt-unicode \
+                    git-core \
+                    build-essential \
+                    virtualbox \
+                    ruby-full
+                    # vagrant ?
