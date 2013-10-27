c550b9f3a4d99214dc9b776bfe7099a3acb36420
Łukasz Korecki <lukasz@coffeesounds.com>
2013-10-27 10:41:13 +0000

    moving main display to the right

--- xres/2ndmonitor.sh
+++ xres/2ndmonitor.sh
@@ -4,5 +4,5 @@ main="LVDS1"
 
 if [[ -z "$(xrandr | grep "$screen disconn")" ]] ; then
   # use 2nd monitor
-  xrandr --output $screen --auto --left-of $main
+  xrandr --output $screen --auto --right-of $main
 fi
