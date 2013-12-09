493c02a339ab259aaceece4eea9a04afe0020948
Łukasz Korecki <lukasz@coffeesounds.com>
2013-12-09 09:10:46 +0000

    Using different port now

--- xres/2ndmonitor.sh
+++ xres/2ndmonitor.sh
@@ -1,8 +1,8 @@
-screen="DP2"
 #!/usr/bin/env bash
+screen="HDMI1"
 main="LVDS1"
 
 if [[ -z "$(xrandr | grep "$screen disconn")" ]] ; then
   # use 2nd monitor
-  xrandr --output $screen --auto --right-of $main
+  xrandr --output $screen --auto --left-of $main
 fi
