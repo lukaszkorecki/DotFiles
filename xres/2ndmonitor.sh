5c6b8b9ddf8af63c8b46a5b098da3e19df80378f
Łukasz Korecki <lukasz@coffeesounds.com>
2014-02-17 09:37:36 +0000

    monitor on the right

--- xres/2ndmonitor.sh
+++ xres/2ndmonitor.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
 screen="HDMI1"
+#!/usr/bin/env bash
 main="LVDS1"
 
 if [[ -z "$(xrandr | grep "$screen disconn")" ]] ; then
   # use 2nd monitor
-  xrandr --output $screen --auto --left-of $main
+  xrandr --output $screen --auto --right-of $main
 fi
