b96b06a98d798f9db1f1cbb75adc7217a0f317e4
Łukasz Korecki <lukasz@korecki.me>
2015-03-02 10:25:28 +0000

    script for setting sane OSX defaults

---
+++ osx.sh
@@ -0,0 +1,6 @@
+sudo defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+sudo defaults write -g NSWindowResizeTime -float 0.003
+sudo defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+sudo defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+sudo defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+sudo defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
