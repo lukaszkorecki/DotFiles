8e56d4cc05dd89ec1ba978769168be7f4379cd8b
Łukasz Korecki <lukasz@korecki.me>
2015-10-16 15:29:06 +0000

    Enable colors in ultra

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,5 @@
-{:user {:plugins [
-                  [ lein-pprint "1.1.1"]
+{:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-kibit "0.1.2"]
                   [venantius/ultra "0.3.4"]
-                  [lein-cljfmt "0.3.0"]
-                  ] }}
+                  [lein-cljfmt "0.3.0"]]
+        :ultra {:color-scheme :solarized_dark}}}
