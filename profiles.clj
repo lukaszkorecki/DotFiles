2423898a08a496809a6e61e67659e6e74f4fc445
Łukasz Korecki <lukasz@korecki.me>
2015-12-05 12:29:19 +0000

    add lein bikeshed to lein profile

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,9 @@
 {:repl {:plugins [[org.clojure/tools.nrepl "0.2.12"]
-                  [cider/cider-nrepl "0.9.1"] ]}}
+                  [cider/cider-nrepl "0.9.1"] ]}
  :user {:plugins [[lein-pprint "1.1.1"]
                   [lein-kibit "0.1.2"]
                   [debug-repl "0.3.2"]
+                  [lein-bikeshed "0.2.0"]
                   [venantius/ultra "0.3.4"]
                   [lein-bikeshed "0.2.0"]
                   [jonase/eastwood "0.2.1"]
