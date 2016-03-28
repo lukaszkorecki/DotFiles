bb9acc15070ebf8af87f23ac6570d7209ea4792a
Łukasz Korecki <lukasz@korecki.me>
2016-03-28 18:30:23 +0000

    Setup exclusions for clojure in lein profile

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,16 @@
 {:repl {:plugins [[org.clojure/tools.nrepl "0.2.12"]
                   [cider/cider-nrepl "0.9.1"] ]}
- :user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-kibit "0.1.2"]
-                  [debug-repl "0.3.2"]
-                  [lein-bikeshed "0.2.0"]
-                  [venantius/ultra "0.3.4"]
-                  [lein-bikeshed "0.2.0"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-cljfmt "0.4.1"]
-                  [lein-cloverage "1.0.6"]]
+ :user {:plugins [[debug-repl "0.3.2"]
+                  [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+
+                  [lein-bikeshed "0.2.0" :exclusions [org.clojure/clojure]]
+
+                  [lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
+
+                  [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
+
+                  [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
+
+                  [lein-pprint "1.1.1"]
+                  [venantius/ultra "0.3.4" :exclusions [org.clojure/clojure]]]
         :ultra {:color-scheme :solarized_dark}}}
