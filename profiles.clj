533bb97e81c29e82b99d6e7401bfb004105b25f6
Łukasz Korecki <lukasz@korecki.me>
2016-04-01 18:17:19 +0100

    Remove unused plugins

--- profiles.clj
+++ profiles.clj
@@ -1,17 +1,19 @@
+;; make this work with latest cider
 {:repl {:plugins [[org.clojure/tools.nrepl "0.2.12"]
                   [cider/cider-nrepl "0.12.0-SNAPSHOT"]]
+        ;; make it accessible from vagrant
         :repl-options { :host "0.0.0.0" :port 4001 }}
- :user {:plugins [[debug-repl "0.3.2"]
+ :user {:plugins [
+                  ;; lint them things
                   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-
-                  [lein-bikeshed "0.2.0" :exclusions [org.clojure/clojure]]
-
-                  [lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
-
-                  [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
-
                   [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
 
-                  [lein-pprint "1.1.1"]
+                  ;; more linting
+                  [lein-bikeshed "0.2.0" :exclusions [org.clojure/clojure]]
+                  ;; can't live without it
+                  [lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
+                  ;; this!
+                  [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
+                  ;; everyone wants a nice repl
                   [venantius/ultra "0.3.4" :exclusions [org.clojure/clojure]]]
         :ultra {:color-scheme :solarized_dark}}}
