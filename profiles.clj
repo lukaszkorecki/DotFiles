413b11858428effe72e0e42ee1e75145e72e6ca0
Łukasz Korecki <lukasz@korecki.me>
2016-04-01 16:14:41 +0000

    Update to latest nrepl and make it work for remote machines

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 {:repl {:plugins [[org.clojure/tools.nrepl "0.2.12"]
-                  [cider/cider-nrepl "0.9.1"] ]}
+                  [cider/cider-nrepl "0.12.0-SNAPSHOT"]]
+        :repl-options { :host "0.0.0.0" :port 4001 }}
  :user {:plugins [[debug-repl "0.3.2"]
                   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
 
