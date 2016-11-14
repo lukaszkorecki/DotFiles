499da4a7b339f013d16e292bb4ecab2f3609cf9b
Łukasz Korecki <lukasz@korecki.me>
2016-11-14 13:21:42 +0000

    bump nrepl

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,6 @@
 ;; make this work with latest cider
 {:repl {:plugins [[org.clojure/tools.nrepl "0.2.12"]
-                  [cider/cider-nrepl "0.12.0-SNAPSHOT"]]
-
+                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]]
         :repl-options {:host "0.0.0.0"
                        :timeout 120000 }}
  :user {:plugins [
