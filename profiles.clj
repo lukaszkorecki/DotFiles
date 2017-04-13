5bfc7adf48fe75ee223486b526744812844b191e
Łukasz Korecki <lukasz@korecki.me>
2017-04-13 15:17:25 +0100

    Add clj-refactor middleware

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 ;; make this work with latest cider
 {:repl {:plugins [[org.clojure/tools.nrepl "0.2.12"]
+                  [refactor-nrepl "2.2.0"]
                   [cider/cider-nrepl "0.14.0-SNAPSHOT"]]
         :repl-options {:host "0.0.0.0"
                        :timeout 120000 }}
