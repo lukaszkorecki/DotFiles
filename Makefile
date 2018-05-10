9c132f6cafc2e40ec398e2b054be533053fd91a8
Łukasz Korecki <lukasz@korecki.me>
2018-05-10 10:43:39 +0100

    remove lein setup, moved to cult-leader

--- Makefile
+++ Makefile
@@ -22,8 +22,6 @@ unlink_dotfiles:
 link_special_dotfiles: private
 	mkdir -p ~/.ssh/
 	ln -fvs ~/.private/ssh/config ~/.ssh/config
-	mkdir -p ~/.lein
-	ln -fvs ~/.DotFiles/profiles.clj ~/.lein/profiles.clj
 
 unlink_special_dotfiles:
 	rm ~/.ssh/config
--- profiles.clj
+++
@@ -1,9 +0,0 @@
-{
-  ;; inf-clojure support
-  :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]
- ;; for all that repl goodness
- :user { :dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]]
-        :plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
-                  [lein-cloverage "1.0.6" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.10", :exclusions [org.clojure/clojure]]]}
-}
